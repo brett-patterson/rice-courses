8fd881514340245c967e0b10f2f0b05471897745
Brett Patterson <brett.patterson0@gmail.com>
2014-11-26 15:50:39 -0600

    Finish filter widget upgrade

--- wsgi/static/js/filterWidget.js
+++ wsgi/static/js/filterWidget.js
@@ -1,16 +1,87 @@
 /**
+ * An input that grows as you type.
+ * Adapted from:
+ * http://stackoverflow.com/questions/931207/is-there-a-jquery-autogrow-plugin-for-text-fields
+ * @constructor
+ * @param {object} options - An optional options object
+ */
+$.fn.autoGrowInput = function(options) {
+    options = $.extend({
+        maxWidth: 1000,
+        minWidth: 0,
+        comfortZone: 70,
+        changeCallback: function() {}
+    }, options);
+
+    this.filter('input:text').each(function() {
+        var minWidth = options.minWidth || $(this).width(),
+            val = '',
+            input = $(this),
+            testSubject = $('<div/>').css({
+                position: 'absolute',
+                top: -9999,
+                left: -9999,
+                width: 'auto',
+                fontSize: input.css('fontSize'),
+                fontFamily: input.css('fontFamily'),
+                fontWeight: input.css('fontWeight'),
+                letterSpacing: input.css('letterSpacing'),
+                whiteSpace: 'nowrap'
+            }),
+            check = function() {
+
+                if (val === (val = input.val())) {return;}
+
+                // Enter new content into testSubject
+                var escaped = val.replace(/&/g, '&amp;')
+                                 .replace(/\s/g, '&nbsp;')
+                                 .replace(/</g, '&lt;')
+                                 .replace(/>/g, '&gt;');
+                testSubject.html(escaped);
+
+                // Calculate new width + whether to change
+                var testerWidth = testSubject.width();
+                var newWidth;
+                if (testerWidth + options.comfortZone >= minWidth)
+                    newWidth = testerWidth + options.comfortZone;
+                else
+                    newWidth = minWidth;
+
+                var currentWidth = input.width();
+                var validChange = (newWidth < currentWidth &&
+                                       newWidth >= minWidth) ||
+                                  (newWidth > minWidth &&
+                                       newWidth < options.maxWidth);
+
+                // Animate width
+                if (validChange) {
+                    input.width(newWidth);
+                    options.changeCallback();
+                }
+
+            };
+
+        testSubject.insertAfter(input);
+
+        $(this).bind('keyup keydown blur update', check);
+        check();
+    });
+
+    return this;
+
+};
+
+/**
  * Create a filter widget.
  * @constructor
- * @param {object} filterKeywords - A mapping of filter keywords to their name
  * @param {object} config - An optional config object
  */
-$.fn.filterWidget = function(filterKeywords, config) {
-    if (filterKeywords === undefined)
-        filterKeywords = {};
-
+$.fn.filterWidget = function(config) {
     var config = $.extend({
         key: ':',
         placeholder: '',
+        filterKeywords: {},
+        filtersChanged: function(filters) {},
         backgroundColor: function(field) { return null },
         textColor: function(field) { return null },
         removeColor: function(field) { return null },
@@ -71,25 +142,32 @@ $.fn.filterWidget = function(filterKeywords, config) {
     };
 
     filterManagerObj.addFilter = function(filter) {
-        filter.id = filterManagerObj.config._filterIndex;
-        filterManagerObj.config._filterIndex++;
+        filter.id = this.config._filterIndex;
+        this.config._filterIndex++;
         this.filters.push(filter);
 
-        var config = filterManagerObj.config;
-        var bgColor = config.backgroundColor(filter.field);
-        var textColor = config.textColor(filter.field);
-        var removeColor = config.removeColor(filter.field);
-        var removeHoverColor = config.removeHoverColor(filter.field);
+        var bgColor = this.config.backgroundColor(filter.field);
+        var textColor = this.config.textColor(filter.field);
+        var removeColor = this.config.removeColor(filter.field);
+        var removeHoverColor = this.config.removeHoverColor(filter.field);
 
         var filterView = $('<span/>', {
             class: 'filter-view',
             id: 'filter-view-' + filter.id,
-            text: filter.name + ' ',
+            text: filter.name + ': ',
             css: {
                 backgroundColor: bgColor,
                 color: textColor
             }
-        }).append($('<a/>', {
+        }).append($('<input/>', {
+            id: 'filter-view-input-' + filter.id,
+            class: 'filter-view-input',
+            width: 10,
+            css: {
+                backgroundColor: bgColor,
+                color: textColor
+            }
+        })).append($('<a/>', {
             href: '#',
             onclick: 'removeFilterClicked(' + filter.id + ')',
             html: '<span class="glyphicon glyphicon-remove"></span>',
@@ -101,13 +179,30 @@ $.fn.filterWidget = function(filterKeywords, config) {
         }, function() {
             $(this).css('color', removeColor);
         })).insertBefore(filterInput).click(function() {
-            filterInput.focus();
+            $('#filter-view-input-' + filter.id).focus();
+        });
+
+        $('#filter-view-input-' + filter.id).focus().on('keydown', function(e) {
+            if (e.keyCode == 13)
+                filterInput.focus();
+            else if (e.keyCode == 8 && $(e.target).val() == '')
+                filterManagerObj.removeFilter(filter);
+        }).autoGrowInput({
+            comfortZone: 10,
+            changeCallback: resizeInput
+        }).bind('input', function(event) {
+            filter.value = $(event.target).val();
+            var index = filterManagerObj.filters.indexOf(filter);
+            filterManagerObj.filters.splice(index, 1, filter);
+            filterManagerObj.config.filtersChanged(filterManagerObj.filters);
         });
 
         resizeInput();
 
         if (this.filters.length == 1)
             filterInput.attr('placeholder', '');
+
+        this.config.filtersChanged(this.filters);
     };
 
     filterManagerObj.removeFilter = function(filter) {
@@ -119,6 +214,8 @@ $.fn.filterWidget = function(filterKeywords, config) {
             if (this.filters.length == 0)
                 filterInput.attr('placeholder',
                                  filterManagerObj.config.placeholder);
+
+            this.config.filtersChanged(this.filters);
     };
 
     window.removeFilterClicked = function(id) {
@@ -129,13 +226,14 @@ $.fn.filterWidget = function(filterKeywords, config) {
     filterManagerObj.onTextChange = function(event) {
         var input = $(event.target);
         var text = input.val();
+        var config = filterManagerObj.config;
 
-        var keyIndex = text.indexOf(filterManagerObj.config.key);
+        var keyIndex = text.indexOf(config.key);
 
         if (keyIndex > -1) {
             var field = text.substring(0, keyIndex).toLowerCase();
             var value = text.substring(keyIndex + 1);
-            var name = filterKeywords[field];
+            var name = config.filterKeywords[field];
             if (field.length > 0 && name !== undefined) {
                 filterManagerObj.addFilter({
                     field: field,
@@ -148,7 +246,7 @@ $.fn.filterWidget = function(filterKeywords, config) {
         }
     };
 
-    filterManagerObj.onKeyUp = function(event) {
+    filterManagerObj.onKeyDown = function(event) {
         var filterLength = filterManagerObj.filters.length;
         if (event.keyCode == 8 && filterLength > 0 &&
             filterManagerObj.inputText().length == 0) {
@@ -158,7 +256,7 @@ $.fn.filterWidget = function(filterKeywords, config) {
     };
 
     filterInput.bind('input', filterManagerObj.onTextChange);
-    filterInput.bind('keyup', filterManagerObj.onKeyUp);
+    filterInput.bind('keydown', filterManagerObj.onKeyDown);
 
     return filterManagerObj;
 };
