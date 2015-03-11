64a0e000a8f42ea9b521c5d6cbde1d7d38654731
Brett Patterson <brett.patterson@rice.edu>
2015-03-11 16:21:45 -0500

    Add full path for wsgi

--- rice_course_manager/wsgi.py
+++ rice_course_manager/wsgi.py
@@ -8,7 +8,7 @@ https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
 """
 import os
 
-os.environ['DJANGO_SETTINGS_MODULE'] = 'settings'
+os.environ['DJANGO_SETTINGS_MODULE'] = 'rice_course_manager.settings'
 
 from django.core.wsgi import get_wsgi_application
 from dj_static import Cling
