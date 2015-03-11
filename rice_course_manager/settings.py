e2deab9516d4ae32031bbfedde487c227b450c5b
Brett Patterson <brett.patterson@rice.edu>
2015-03-11 16:37:52 -0500

    Fix STATIC_ROOT url

--- rice_course_manager/settings.py
+++ rice_course_manager/settings.py
@@ -108,7 +108,7 @@ USE_TZ = False
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 STATIC_URL = '/static/'
-STATIC_ROOT = 'staticfiles'
+STATIC_ROOT = '/app/staticfiles/'
 STATICFILES_DIRS = (
     os.path.join(os.path.dirname(BASE_DIR), 'static'),
 )
