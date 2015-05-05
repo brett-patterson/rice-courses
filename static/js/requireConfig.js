50d59ae52089ff6b06d259b2bc02caf9d9869946
Brett Patterson <brett.patterson@rice.edu>
2015-05-05 15:13:19 -0500

    Remove reactable from RequireJS config

--- static/js/requireConfig.js
+++ static/js/requireConfig.js
@@ -10,7 +10,6 @@ require.config({
         moment: "../../lib/moment/moment",
         react: "../../lib/react/react-with-addons",
         reactBootstrap: "../../lib/react-bootstrap/react-bootstrap",
-        reactable: "../../lib/reactable/build/reactable",
         reactDnd: "../../lib/react-dnd/dist/ReactDND.min",
         tutorialize: "../../lib/tutorialize/dist/js/tutorialize",
         zeroClipboard: "../../lib/zeroclipboard/dist/ZeroClipboard"
