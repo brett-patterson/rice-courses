dcdc39fbf0141270bb0e9958a5e0539029d24dde
Brett Patterson <brett.patterson@rice.edu>
2015-05-05 21:04:46 -0500

    Add custom migration for credits column conversion

--- rice_course_manager/courses/migrations/0015_auto_20150505_1644.py
+++ rice_course_manager/courses/migrations/0015_auto_20150505_1644.py
@@ -5,6 +5,9 @@ from django.db import models, migrations
 import courses.fields
 
 
+alter_credits_sql = "ALTER TABLE courses_course ALTER COLUMN credits TYPE numrange USING CASE WHEN strpos(credits, 'to')=0 THEN ('['||credits||','||credits||']')::numrange ELSE ('['||substr(credits, 1, strpos(credits, 'to')-1)||','||substr(credits, strpos(credits, 'to')+2)||']')::numrange END"
+
+
 class Migration(migrations.Migration):
 
     dependencies = [
@@ -12,6 +15,7 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
+        migrations.RunSQL(alter_credits_sql),
         migrations.AlterField(
             model_name='course',
             name='credits',
