4b524a04a1a5e426b05657b0687c4686c658dc5e
Brett Patterson <brett.patterson@rice.edu>
2016-01-14 10:35:36 -0600

    Remove requirements urls

--- rice_course_manager/rice_courses/urls.py
+++ rice_course_manager/rice_courses/urls.py
@@ -11,6 +11,5 @@ urlpatterns = patterns('',
     url(r'^evaluation/', include('evaluation.urls')),
     url(r'^help/', include('help.urls')),
     url(r'^me/', include('me.urls')),
-    url(r'^requirements/', include('requirements.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
